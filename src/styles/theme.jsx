7cd81712555884f05d4fb516be52517b894dafdf
task11 <task11@naver.com>
2022-11-02 03:56:04 +0900

    feat: theme 선언 및 themeProvider 연결

---
+++ src/styles/theme.jsx
@@ -0,0 +1,20 @@
+const defaultTheme = {
+  textColor: {
+    default: '#000000',
+    accent: '#FFFFFF'
+  },
+  btnColor: {
+    default: '#D9D9D9',
+    active: '#000000',
+    accent: '#0094FF'
+  },
+  bgColor: {
+    default: 'white',
+    accent: '#0094FF'
+  },
+  border: {
+    default: '#000000'
+  }
+};
+
+export default defaultTheme;
