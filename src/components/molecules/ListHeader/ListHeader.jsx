ed9bdbbcd610ee12e16c426b0ac23aa481d8c197
task11 <task11@naver.com>
2022-11-02 21:19:00 +0900

    feat: list header moledules 구현

---
+++ src/components/molecules/ListHeader/ListHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Atoms from '../../atoms';
+
+export default function ListHeader(props) {
+  const { children } = props;
+
+  return (
+    <Atoms.ListHeaderLayout>
+      <Atoms.TextContent size='md' variant='accent'>
+        {children}
+      </Atoms.TextContent>
+    </Atoms.ListHeaderLayout>
+  );
+}
