9fa5934e15d1297e89f6d12c5bb4419038e7a948
task11 <task11@naver.com>
2022-11-02 22:12:22 +0900

    refactor: molecules 폴더 구조 변경

--- src/components/molecules/CardTitle.jsx
+++ src/components/molecules/CardTitle.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Atoms from '../../atoms';
+import Atoms from '../atoms';
 
 export default function CardTitle(props) {
   const { brand, name, classType, fuelType, amount } = props;
