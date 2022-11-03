f664a6ac3e3d85386744c9e0ff6acc0a0c674342
task11 <task11@naver.com>
2022-11-03 10:54:58 +0900

    feat: 디테일 페이지에 helmet을 이용한 seo 처리

--- src/components/pages/DetailPage.jsx
+++ src/components/pages/DetailPage.jsx
@@ -21,46 +21,53 @@ export default function Detail() {
   return (
     <>
       {isLoading ? (
-        <Templates.Detail>
-          <Atoms.Image src={carDetail.attribute.imageUrl} size='md' />
-          <Molecules.ListTitle
-            brand={carDetail.attribute.brand}
-            name={carDetail.attribute.name}
+        <>
+          <Templates.Detail>
+            <Atoms.Image src={carDetail.attribute.imageUrl} size='md' />
+            <Molecules.ListTitle
+              brand={carDetail.attribute.brand}
+              name={carDetail.attribute.name}
+            />
+            <Molecules.ListItem
+              left=''
+              right={`월 ${addCommas(carDetail.amount)}원`}
+            />
+            <Molecules.ListHeader>차량 정보</Molecules.ListHeader>
+            <Molecules.ListItem
+              left='차종'
+              right={SEGMENT[carDetail.attribute.segment]}
+            />
+            <Molecules.ListItem
+              left='연료'
+              right={FUELTYPE[carDetail.attribute.fuelType]}
+            />
+            <Molecules.ListItem
+              left='이용 가능일'
+              right={`${formatDate(carDetail.startDate)} 부터`}
+            />
+            <Molecules.ListHeader>보험</Molecules.ListHeader>
+            {carDetail.insurance.map(({ name, description }, idx) => (
+              <Molecules.ListItem key={idx} left={name} right={description} />
+            ))}
+            {checkLength(carDetail.additionalProducts) && (
+              <>
+                <Molecules.ListHeader>추가상품</Molecules.ListHeader>
+                {carDetail.additionalProducts.map(({ name, amount }, idx) => (
+                  <Molecules.ListItem
+                    key={idx}
+                    left={name}
+                    right={`월 ${addCommas(amount)}원`}
+                  />
+                ))}
+              </>
+            )}
+          </Templates.Detail>
+          <Molecules.ReactHelmet
+            title={`${carDetail.attribute.brand} ${carDetail.attribute.name}`}
+            description={`월 ${addCommas(carDetail.amount)}원`}
+            imageUrl={carDetail.attribute.imageUrl}
           />
-          <Molecules.ListItem
-            left=''
-            right={`월 ${addCommas(carDetail.amount)}원`}
-          />
-          <Molecules.ListHeader>차량 정보</Molecules.ListHeader>
-          <Molecules.ListItem
-            left='차종'
-            right={SEGMENT[carDetail.attribute.segment]}
-          />
-          <Molecules.ListItem
-            left='연료'
-            right={FUELTYPE[carDetail.attribute.fuelType]}
-          />
-          <Molecules.ListItem
-            left='이용 가능일'
-            right={`${formatDate(carDetail.startDate)} 부터`}
-          />
-          <Molecules.ListHeader>보험</Molecules.ListHeader>
-          {carDetail.insurance.map(({ name, description }, idx) => (
-            <Molecules.ListItem key={idx} left={name} right={description} />
-          ))}
-          {checkLength(carDetail.additionalProducts) && (
-            <>
-              <Molecules.ListHeader>추가상품</Molecules.ListHeader>
-              {carDetail.additionalProducts.map(({ name, amount }, idx) => (
-                <Molecules.ListItem
-                  key={idx}
-                  left={name}
-                  right={`월 ${addCommas(amount)}원`}
-                />
-              ))}
-            </>
-          )}
-        </Templates.Detail>
+        </>
       ) : (
         <Organisms.Loading />
       )}
